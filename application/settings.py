a474cda772934e89e1efffc85eb5401f9926e238
sahasrara62 <nazi1062>
2020-12-04 20:35:28 +0530

    added environment variable to run the application better

--- application/settings.py
+++ application/settings.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Application configuration.
+
+Most configuration is set via environment variables.
+
+For local development, use a .env file to set
+environment variables.
 """
-File to store all the information related to environment variable if require
-"""
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+FLASK_APP = env.str("FLASK_APP")
+SECRET = env.str("SECRET")
+APP_SETTINGS = env.str("APP_SETTINGS")
+FLASK_RUN_HOST = env.str("FLASK_RUN_HOST")
+FLASK_RUN_PORT = env.int("FLASK_RUN_PORT")
+
