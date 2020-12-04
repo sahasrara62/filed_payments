f80340f0c91bb485c9b42ffe07798e274484f3e5
sahasrara62 <nazi1062>
2020-12-04 20:34:51 +0530

    updated apppy config, added the blueprint to handle url better

--- application/app.py
+++ application/app.py
@@ -1,9 +1,17 @@
 from typing import Any
-
 from flask import Flask
+from application import views
 
+# see flask rest api for app initialization
 
 def create_app(config_object="application.settings"):
-	app = Flask(__name__.split(".")[0])
+	app = Flask(__name__)
 	app.config.from_object(config_object)
+	register_blueprints(app)
 	return app
+
+
+def register_blueprints(app):
+	"""Register Flask blueprints."""
+	app.register_blueprint(views.views.blueprint)
+	return None
