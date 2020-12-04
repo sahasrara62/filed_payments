e35f53ee0d9fc4e918457595559fe1ebe3d16f59
sahasrara62 <nazi1062>
2020-12-04 20:36:47 +0530

    __init__ file to handle the testing

---
+++ application/test/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Defines fixtures available to all tests."""
+
+import pytest
+from webtest import TestApp
+
+from application.app import create_app
+
+
+
+@pytest.fixture
+def app():
+    """Create application for the tests."""
+    _app = create_app("test.settings")
+    ctx = _app.test_request_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+@pytest.fixture
+def testapp(app):
+    """Create Webtest app."""
+    return TestApp(app)
